9b407083a854e2ea7f2dfd0c44ed2a4a93cb7642
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-15 17:44:06 -0600

    Ordenamiento burbuja (clásico)

---
+++ BubbleSort/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include "functions.h"
+
+int i;
+
+int
+main( void )
+{
+	int A[1000];
+	fillArray( A );
+	bubbleSort( A );
+}
+
+
